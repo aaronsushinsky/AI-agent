0c6dd98128334413300efdc4df92e2c395069cf4
Aaron Sushinsky <aaronsushinsky@gmail.com>
2026-01-08 16:53:33 -0600

    Added verbose option

--- main.py
+++ main.py
@@ -15,16 +15,19 @@ def main():
 
     parser = argparse.ArgumentParser(description="Chatbot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
-    
+
     response = client.models.generate_content(
     model='gemini-2.5-flash', contents=messages
     #args.user_prompt
     )
     if response.usage_metadata != None:
-        print("Prompt tokens: ", response.usage_metadata.prompt_token_count)
-        print("Response tokens: ", response.usage_metadata.candidates_token_count)
+        if args.verbose == True:
+            print("Prompt tokens: ", response.usage_metadata.prompt_token_count)
+            print("Response tokens: ", response.usage_metadata.candidates_token_count)
+            print("User prompt: ", args.user_prompt)
         print(response.text)
     else:
         raise RuntimeError
