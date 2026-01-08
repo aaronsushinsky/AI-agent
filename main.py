56259fd8b899b977c4cd5db371b27ec4833a9837
Aaron Sushinsky <aaronsushinsky@gmail.com>
2026-01-08 09:31:22 -0600

    API access set up, test prompts, and token tracking

---
+++ main.py
@@ -0,0 +1,33 @@
+import os
+import argparse
+from dotenv import load_dotenv
+from google import genai
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+if api_key == None:
+    raise RuntimeError
+
+from google.genai import types
+client = genai.Client(api_key=api_key)
+
+def main():
+
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+    
+    response = client.models.generate_content(
+    model='gemini-2.5-flash', contents=messages
+    #args.user_prompt
+    )
+    if response.usage_metadata != None:
+        print("Prompt tokens: ", response.usage_metadata.prompt_token_count)
+        print("Response tokens: ", response.usage_metadata.candidates_token_count)
+        print(response.text)
+    else:
+        raise RuntimeError
+
+if __name__ == "__main__":
+    main()
